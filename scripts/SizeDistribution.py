526fc4a36e12b62d31c4b9823b1b2b1b3506b390
Andrew Kravchuk <awkravchuk@gmail.com>
2014-12-02 11:48:30 +0300

    Improved script for building size distribution histogram

--- scripts/SizeDistribution.py
+++ scripts/SizeDistribution.py
@@ -5,14 +5,21 @@ import sqlite3
 import matplotlib.pyplot as plt
 
 
-def usage(argv):
-	print "USAGE: " + argv[0] + " [dataset path]"
-
 def main(argv):
-	if len(argv) > 2:
-		usage(argv)
-		return 1
-	path = '.' if len(argv) == 1 else argv[1]
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument('path', help='path to dataset folder',
+		default='.', nargs='?', metavar='path')
+	parser.add_argument('-p', '--predicate', help='SQL predicate for objects to plot',
+		type=str)
+	plot_group = parser.add_mutually_exclusive_group()
+	plot_group.add_argument('-W', '--width', help='only plot width',
+		action='store_true')
+	plot_group.add_argument('-H', '--height', help='only plot height',
+		action='store_true')
+	args = parser.parse_args()
+
+	path = args.path
 	filename = os.path.join(path, 'index.sqlite3')
 	if not os.path.exists(filename):
 		print "No dataset found in directory '" + path + "'"
@@ -21,10 +28,19 @@ def main(argv):
 	conn = sqlite3.connect(filename)
 	cur = conn.cursor()
 	data = {}
-	for row in cur.execute('select width, height from objects'):
-		width = row[0]
+	query = 'select width, height from objects'
+	if args.predicate is not None:
+		query += ' where ' + args.predicate
+	for row in cur.execute(query):
+		width  = row[0]
 		height = row[1]
-		size = width*height
+		if args.width:
+			size = width
+		elif args.height:
+			size = height
+		else:
+			size = width * height
+
 		if size not in data:
 			data[size] =  1
 		else:
@@ -35,7 +51,7 @@ def main(argv):
 	plt.show()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	import sys
 	exit(main(sys.argv))
 
